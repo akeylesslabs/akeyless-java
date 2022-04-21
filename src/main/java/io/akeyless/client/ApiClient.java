414a21541466c33e9facf00fa6cfd308e467cf06
akeyless <github@akeyless.io>
2022-04-21 11:39:27 +0000

    Release v2.16.3

--- src/main/java/io/akeyless/client/ApiClient.java
+++ src/main/java/io/akeyless/client/ApiClient.java
@@ -108,7 +108,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/2.16.2/java");
+        setUserAgent("OpenAPI-Generator/2.16.3/java");
 
         authentications = new HashMap<String, Authentication>();
     }
