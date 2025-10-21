09a030398e23e2f7cfa071b2d847c03e6d013ccc
akeyless <github@akeyless.io>
2025-10-21 14:38:33 +0000

    "Release v5.0.9"

--- src/main/java/io/akeyless/client/ApiClient.java
+++ src/main/java/io/akeyless/client/ApiClient.java
@@ -139,7 +139,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/5.0.11/java");
+        setUserAgent("OpenAPI-Generator/5.0.9/java");
 
         authentications = new HashMap<String, Authentication>();
     }
--- src/main/java/io/akeyless/client/Configuration.java
+++ src/main/java/io/akeyless/client/Configuration.java
@@ -15,7 +15,7 @@ package io.akeyless.client;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.10.0")
 public class Configuration {
-    public static final String VERSION = "5.0.11";
+    public static final String VERSION = "5.0.9";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
